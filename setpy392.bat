22d697b040aa0ef01e9b8e292e60b27aa4a310f6
Smarter Worker <smarterworkeruk@gmail.com>
2025-06-27 11:32:49 +0100

    Set envirn
    Batch files used to start flask

---
+++ setpy392.bat
@@ -0,0 +1,6 @@
+h:\
+SET PYTHON_VERS=Python392
+SET PYTHON_HOME=H:\APPS\Python39
+set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Lib;%PYTHON_HOME%;%PYTHON_HOME%\scripts;;%PATH%
+set PYTHONPATH=%PYTHONPATH%;H:\Workarea\DataB\projects\bitbucket\python\learning;%PYTHON_HOME%\lib\site-packages;
+cd %PYTHON_HOME%
---
+++ start_flask20.bat
@@ -0,0 +1,6 @@
+title Python3.9.13 Flask2.0 env %1
+h:
+cd h:\%1
+call setpy392.bat
+call H:\workarea\DataB\projects\gen\python\py_envs\flask20\Scripts\activate.bat
+cd h:\%1
